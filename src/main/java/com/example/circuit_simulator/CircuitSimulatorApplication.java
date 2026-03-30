3fe0b9eee49ac9a210bd36763890c2771870c3c4
lkhok22 <lkhok22@freeuni.edu.ge>
2026-03-30 18:31:28 +0400

    first-commit

---
+++ src/main/java/com/example/circuit_simulator/CircuitSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.circuit_simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitSimulatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/circuit_simulator/CircuitSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.circuit_simulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
